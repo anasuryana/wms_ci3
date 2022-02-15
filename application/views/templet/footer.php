09065ed89268beae10b40b07145131179a4d0a78
Ana <anasuryana10@gmail.com>
2022-02-15 09:33:12 +0700

    include jspreadsheet library

--- application/views/templet/footer.php
+++ application/views/templet/footer.php
@@ -281,36 +281,7 @@ $("#FOOTER_MODWH_btnsave").click(function (e) {
                 showinfoofus_FG();
             }
         }
-    })  
-    $(document).unbind('keydown').bind('keydown', function (event) {        
-        switch(event.keyCode){   
-            case 8:       
-                var doPrevent = true;
-                var types = ["text", "password", "file", "search", "email", "number", "date", "color", "datetime", "datetime-local", "month", "range", "search", "tel", "time", "url", "week"];
-                var d = $(event.srcElement || event.target);
-                var disabled = d.prop("readonly") || d.prop("disabled");
-                if (!disabled) {
-                    if (d[0].isContentEditable) {                
-                        doPrevent = false;
-                    } else if (d.is("input")) {
-                        var type = d.attr("type");
-                        if (type) {
-                            type = type.toLowerCase();                    
-                        }
-                        if (types.indexOf(type) > -1) {                    
-                            doPrevent = false;
-                        }
-                    } else if (d.is("textarea")) {                
-                        doPrevent = false;
-                    }
-                }
-                if (doPrevent) {
-                    event.preventDefault();
-                    return false;
-                }
-                break;
-        }
-    })
+    })      
     function cmpr_selectElementContents(el) {
         let body = document.body, range, sel;
         if (document.createRange && window.getSelection) {
