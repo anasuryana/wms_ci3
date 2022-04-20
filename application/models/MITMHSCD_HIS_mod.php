73f3cbcdc475c24d59f01fbf94dbe12c3b1965f5
Ana <anasuryana10@gmail.com>
2022-04-20 11:04:45 +0700

    new model for HSCD History

---
+++ application/models/MITMHSCD_HIS_mod.php
@@ -0,0 +1,24 @@
+<?php 
+class MITMHSCD_HIS_mod extends CI_Model {
+    public function __construct()
+    {
+        $this->load->database();        
+    }
+
+    public function insert($data)
+    {
+        $this->db->insert($this->TABLENAME,$data);
+        return $this->db->affected_rows();
+    }
+    public function lastserialid($pdoc){
+        $qry = "select MAX(MITMHSCD_HIS_LINE) lser from $this->TABLENAME 
+        WHERE MITMHSCD_HIS_ITMCD=?";
+        $query =  $this->db->query($qry, [$pdoc]);  
+        if ($query->num_rows()>0){
+            $ret = $query->row();
+            return $ret->lser;
+        } else {
+            return '0';
+        }
+    }
+}
