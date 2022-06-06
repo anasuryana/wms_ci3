aaf36c76329ffa7678e185eaa7eceb909de47e09
Ana <anasuryana10@gmail.com>
2022-06-06 13:02:03 +0700

    add new routes

--- application/config/routes.php
+++ application/config/routes.php
@@ -91,6 +91,7 @@ $route['conversion_test_doc_as_xls'] = 'SER/conversion_test_as_xls';
 $route['kka_mega_as_xls'] = 'ITH/report_kka_mega';
 $route['po_list'] = 'PO/report';
 $route['master_hscode_as_xls'] = 'MSTITM/searchrm_exim_xls';
+$route['master_hscode_fg_as_xls'] = 'MSTITM/searchfg_exim_xls';
 $route['logx/(:any)'] = 'ITH/getlogexport/$1';
 $route['404_override'] = '';
 $route['translate_uri_dashes']  = FALSE;
