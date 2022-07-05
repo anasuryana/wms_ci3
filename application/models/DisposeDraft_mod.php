b537aa6d948a63cdf62ff96e9c5d131dfa12b4ea
Ana <anasuryana10@gmail.com>
2022-07-05 13:04:24 +0700

    Dispose Draft Model

--- application/models/DisposeDraft_mod.php
+++ application/models/DisposeDraft_mod.php
@@ -30,24 +30,45 @@ class DisposeDraft_mod extends CI_Model {
         // )
         // GROUP BY ITEM
         // ORDER BY ITEM";
+        // $qry = "SELECT ITEM PART_CODE,sum(TQTY) QTY FROM 
+        // (select ISNULL(MITMGRP_ITMCD,PART_CODE) ITEM,SUM(QTY) TQTY from DISPOSEDRAFT 
+        // LEFT JOIN MITMGRP_TBL ON PART_CODE=MITMGRP_ITMCD_GRD        
+        // GROUP BY MITMGRP_ITMCD,PART_CODE) VITEM
+        // WHERE ITEM IN (
+        //     '128385000',
+        //     '128430000',
+        //     '218773700',
+        //     '4220029800',
+        //     '8069156400',
+        //     'E06166',
+        //     'E09573',
+        //     'E12394',
+        //     'M81417-05',
+        //     'M81422-06',
+        //     'M81511-01',
+        //     'M89024-02'         
+        // )
+        // GROUP BY ITEM
+        // ORDER BY ITEM";        
         $qry = "SELECT ITEM PART_CODE,sum(TQTY) QTY FROM 
         (select ISNULL(MITMGRP_ITMCD,PART_CODE) ITEM,SUM(QTY) TQTY from DISPOSEDRAFT 
         LEFT JOIN MITMGRP_TBL ON PART_CODE=MITMGRP_ITMCD_GRD        
         GROUP BY MITMGRP_ITMCD,PART_CODE) VITEM
         WHERE ITEM IN (
-            '128385000',
-            '128430000',
-            '218773700',
-            '4220029800',
-            '8069156400',
-            'E06166',
-            'E09573',
-            'E12394',
-            'M81417-05',
-            'M81422-06',
-            'M81511-01',
-            'M89024-02'         
+            '2039838-2',
+            '211395100'            
         )
+        GROUP BY ITEM
+        ORDER BY ITEM";
+		$query = $this->db->query($qry);
+        return $query->result_array();
+    }
+
+    public function select_resume_rm_additional1(){
+        $qry = "SELECT ITEM PART_CODE,sum(TQTY) QTY FROM 
+        (select ISNULL(MITMGRP_ITMCD,PART_CODE) ITEM,SUM(QTY) TQTY from DISPOSEDRAFT2
+        LEFT JOIN MITMGRP_TBL ON PART_CODE=MITMGRP_ITMCD_GRD        
+        GROUP BY MITMGRP_ITMCD,PART_CODE) VITEM        
         GROUP BY ITEM
         ORDER BY ITEM";        
 		$query = $this->db->query($qry);
@@ -67,6 +88,19 @@ class DisposeDraft_mod extends CI_Model {
         GROUP BY ITH_ITMCD,SERD2_ITMCD
         ORDER BY ITH_ITMCD,SERD2_ITMCD ASC";        
 		$query = $this->db->query($qry, [$pDate]);
+        return $query->result_array();
+    }
+    public function select_resume_fg_dedicated($pSER){
+        $qry = "SELECT ITH_ITMCD,RTRIM(SERD2_ITMCD) PART_CODE,round(SUM(RMQT),0) QTY FROM
+        (select '' ITH_WH,SER_ITMID ITH_ITMCD,MITM_ITMD1,MITM_SPTNO,SER_QTY STOCKQTY,MITM_STKUOM,SER_ID ITH_SER,SER_DOC,SER_LUPDT ITH_LUPDT
+                from SER_TBL a 
+                INNER join MITM_TBL b on a.SER_ITMID=b.MITM_ITMCD
+                WHERE SER_ID IN ($pSER)
+                ) VDETAIL
+        LEFT JOIN (SELECT SERD2_SER,SERD2_ITMCD,SUM(SERD2_QTY) RMQT FROM SERD2_TBL GROUP BY SERD2_SER,SERD2_ITMCD) VSERD2 ON ITH_SER=SERD2_SER
+        GROUP BY ITH_ITMCD,SERD2_ITMCD
+        ORDER BY ITH_ITMCD,SERD2_ITMCD ASC";        
+		$query = $this->db->query($qry);
         return $query->result_array();
     }
 }
