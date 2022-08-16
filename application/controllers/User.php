f975d67ec1fa54677f9224af256dca0b6137b87a
Ana <anasuryana10@gmail.com>
2022-08-16 07:10:44 +0700

    change password requirement on register user

--- application/controllers/User.php
+++ application/controllers/User.php
@@ -142,26 +142,32 @@ class User extends CI_Controller {
         $this->load->view('UserMGR/vuser_reg',$data);
     }
 
+    function userGroup() {
+        header('Content-Type: application/json');
+        $rs = $this->Usergroup_mod->selectAll();
+        die(json_encode(['data' => $rs]));
+    }
+
     function register(){
         date_default_timezone_set('Asia/Jakarta');
         $currrtime = date('Y-m-d H:i:s');
-        $data = ['lower(MSTEMP_ID)' => strtolower($this->input->get('inUsrid'))];
+        $data = ['lower(MSTEMP_ID)' => strtolower($this->input->post('inUsrid'))];
         if ($this->Usr_mod->check_Primary($data)>0)
         {
             echo "ada";
         } else {
             $data2 = [
-                'MSTEMP_ID' => $this->input->get('inUsrid'),
-                'MSTEMP_PW' => hash('sha256',$this->input->get('inPW')),
-                'MSTEMP_PWHT' => $this->input->get('inPW'),
-                'MSTEMP_FNM' => $this->input->get('inNMF'),
-                'MSTEMP_LNM' => $this->input->get('inNML'),
+                'MSTEMP_ID' => $this->input->post('inUsrid'),
+                'MSTEMP_PW' => hash('sha256',$this->input->post('inPW')),
+                'MSTEMP_PWHT' => $this->input->post('inPWHT'),
+                'MSTEMP_FNM' => $this->input->post('inNMF'),
+                'MSTEMP_LNM' => $this->input->post('inNML'),
                 'MSTEMP_REGTM' => $currrtime,
                 'MSTEMP_IP' => $this->input->ip_address(),
                 'MSTEMP_ACTSTS' => TRUE,
                 'MSTEMP_STS' => TRUE,
                 'MSTEMP_ACTTM' => $currrtime,
-                'MSTEMP_GRP' => $this->input->get('inUsrGrp')
+                'MSTEMP_GRP' => $this->input->post('inUsrGrp')
             ];
             $h = $this->Usr_mod->insert($data2);
             echo $h;
--- application/views/UserMGR/vuser_reg.php
+++ application/views/UserMGR/vuser_reg.php
@@ -1,9 +1,9 @@
 <div style="padding: 10px">
-    <div class="container-xxl">			
+    <div class="container-fluid">
         <div class="row">
             <div class="col-md-4 mb-3">
                 <label for="firstName">Userid</label>
-                <input type="text" class="form-control" id="txtuserid_reg"required maxlength="10">
+                <input type="text" class="form-control" id="txtuserid_reg"required maxlength="9">
             </div>
             <div class="col-md-4 mb-3">
                 <label for="firstName">First Name</label>
@@ -17,26 +17,41 @@
         <div class="row">
             <div class="col-md-6 mb-3">
                 <label for="firstName">Password</label>
-                <input type="password" class="form-control" id="txtuserpw_reg"required maxlength="10">
+                <input type="password" class="form-control" id="txtuserpw_reg"required onkeyup="txtuserpw_reg_eKeyUp(event)">
             </div>
-            <div class="col-md-6 mb-3">
-                <label for="firstName">Confirm password</label>
-                <input type="password" class="form-control" id="txtuserpwconf_reg"required maxlength="40">
-            </div>            
+            <div class="col-md-6 mb-3" id="txtuserpw_div" >
+            </div>
         </div>
         <div class="row">
             <div class="col-md-12 mb-3">
-                <div class="input-group mb-2">                    
-                    <span class="input-group-text" >Group</span>                    
+                <label for="firstName">Confirm password</label>
+                <input type="password" class="form-control" id="txtuserpwconf_reg"required maxlength="40">
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-6 mb-3">
+                <label for="firstName"><span class="fas fa-mobile-retro"></span> Handy Terminal Password</label>
+                <input type="password" class="form-control" id="txtuserpw_reg_ht" required maxlength="8">
+            </div>
+            <div class="col-md-6 mb-3" >
+                <label for="firstName"><span class="fas fa-mobile-retro"></span> Confirm Handy Terminal Password</label>
+                <input type="password" class="form-control" id="txtuserpwconf_reg_ht" required maxlength="8">
+            </div>
+        </div>    
+        <div class="row">
+            <div class="col-md-12 mb-3">
+                <div class="input-group mb-2">
+                    <span class="input-group-text" >Group</span>
                     <select class="form-select" id="cmbGroup_reg" required>
                     <?php foreach ($lusergroup as $r) { ?>
                     <option value="<?php echo $r['MSTGRP_ID']; ?>"> <?php echo $r['MSTGRP_NM']; ?> </option>
                     <?php } ?>
                     </select>
+                    <button id="btnsync_reg" onclick="btnsync_reg_eClick()" class="btn btn-lg btn-primary btn-sm"><i class="fas fa-sync"></i></button>
                 </div>
             </div>
         </div>
-        <div class="row">				
+        <div class="row">
             <div class="col-md-12 mb-3">
             <button id="btnSaveUser_reg" class="btn btn-lg btn-primary btn-sm" type="submit"><i class="fas fa-save"></i> Save</button>
             </div>
@@ -44,47 +59,113 @@
 
     </div>
 </div>
-<script>    
+<script>
+    function btnsync_reg_eClick() {
+        $.ajax({            
+            url: "<?=base_url('User/userGroup')?>",
+            dataType: "json",
+            success: function (response) {
+                const ttlrows = response.data.length
+                let strData = ``
+                for(let i=0;i<ttlrows; i++) {
+                    strData += `<option value="${response.data[i].MSTGRP_ID}">${response.data[i].MSTGRP_NM}</option>`
+                }
+                cmbGroup_reg.innerHTML = strData
+                cmbGroup_reg.focus()
+            }, error:function(xhr,ajaxOptions, throwError) {
+                alert(throwError);
+            }
+        })
+    }
+
+    function txtuserpw_reg_eKeyUp(e){
+        let statusPW = smtPWValidator(e.target.value)
+        if(statusPW.cd==='1') {
+            txtuserpw_div.innerHTML = `<span class="badge bg-success">${statusPW.msg}</span>`
+        } else {
+            txtuserpw_div.innerHTML = `<span class="badge bg-warning">${statusPW.msg}</span>`
+        }
+    }    
 	$('#btnSaveUser_reg').click( function() {		
-		var quserid = $("#txtuserid_reg").val();
-        var qfirstname = $("#txtuserfirstname_reg").val();
-        var qlastname = $("#txtuserlastname_reg").val();
-        var qpw1 = $("#txtuserpw_reg").val();
-        var qpw2 = $("#txtuserpwconf_reg").val();
-        var grp = $("#cmbGroup_reg").val();
+		const quserid = $("#txtuserid_reg").val();
+        const qfirstname = $("#txtuserfirstname_reg").val();
+        const qlastname = $("#txtuserlastname_reg").val();
+        const qpw1 = $("#txtuserpw_reg").val();
+        const qpw2 = $("#txtuserpwconf_reg").val();
+        const grp = $("#cmbGroup_reg").val();
+        const htpw = txtuserpw_reg_ht.value
+        const htpw_c = txtuserpwconf_reg_ht.value
         if (quserid==''){
             $("#txtuserid_reg").focus();
             return;
         }
-        if(qpw1=='' || qpw2==''){
+        if(qpw1==''){
             alert('password should not be empty');
             $("#txtuserpw_reg").focus();
             return ;
         }
-        if(qpw1!=qpw2){
-            alert('please confirm password');
+        if(qpw2==''){
+            alert('password should not be empty');
+            $("#txtuserpwconf_reg").focus();
             return ;
-        }        
-        jQuery.ajax({
-            type: "get",
-            url: "<?php echo base_url();?>" + "index.php/User/register",
-            dataType: "text",
-            data: {inUsrid: quserid , inNMF : qfirstname, inNML : qlastname, inUsrGrp: grp, inPW: qpw1 },
-            success:function(response) {                
-                if (response=='1'){
-                    swal.fire(
-                        'Good',
-                        'registered successfully',
-                        'success'
-                    );
-                } else if (response=="ada") {
-                    alert('the user id is already exist');
+        }
+
+
+        if(qpw1!=qpw2){
+            alert('please confirm password')
+            txtuserpwconf_reg.focus()
+            return ;
+        }
+        if(htpw=='' || htpw_c==''){
+            alert('password should not be empty');
+            txtuserpw_reg_ht.focus();
+            return ;
+        }
+        if(htpw!=htpw_c){
+            alert('please confirm password')
+            txtuserpwconf_reg_ht.focus()
+            return ;
+        }
+
+        //validate WEBApp password
+        let statusPW = smtPWValidator(qpw1)
+        if(statusPW.cd==='1') {
+            txtuserpw_div.innerHTML = `<span class="badge bg-success">${statusPW.msg}</span>`
+        } else {
+            txtuserpw_div.innerHTML = `<span class="badge bg-warning">${statusPW.msg}</span>`
+            txtuserpw_reg.focus()
+            return 
+        }
+
+        //validate HTApp password
+        if(htpw.length<7) {
+            alertify.warning(`at least 7 characters`)
+            txtuserpw_reg_ht.focus()
+            return
+        }
+        if(confirm(`Are you sure ?`)) {
+            jQuery.ajax({
+                type: "POST",
+                url: "<?=base_url("User/register")?>" ,
+                dataType: "text",
+                data: {inUsrid: quserid , inNMF : qfirstname, inNML : qlastname, inUsrGrp: grp, inPW: qpw1
+                , inPWHT:htpw },
+                success:function(response) {
+                    if (response=='1'){
+                        swal.fire(
+                            'Good',
+                            'registered successfully',
+                            'success'
+                        );
+                    } else if (response==="ada") {
+                        alert('the user id is already exist');
+                    }
+                },
+                error:function(xhr,ajaxOptions, throwError) {
+                    alert(throwError);
                 }
-            },
-            error:function(xhr,ajaxOptions, throwError) {
-                alert(throwError);
-            }
-        });
+            })
+        }
 	});	
 
 </script>
