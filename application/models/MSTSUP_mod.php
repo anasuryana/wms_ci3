ed9becb243dc56d450f5f989590c3d904ee91cbb
Ana <anasuryana10@gmail.com>
2022-08-16 07:53:55 +0700

    trim data npwp

--- application/models/MSTSUP_mod.php
+++ application/models/MSTSUP_mod.php
@@ -30,7 +30,7 @@ class MSTSUP_mod extends CI_Model {
 	}
 
 	public function select_like($plike){
-		$this->db->select("MSUP_SUPCD,MSUP_SUPCR,MSUP_SUPNM,MSUP_ABBRV,RTRIM(MSUP_ADDR1) MSUP_ADDR1,MSUP_TELNO,MSUP_FAXNO,MSUP_TAXREG");
+		$this->db->select("MSUP_SUPCD,MSUP_SUPCR,MSUP_SUPNM,MSUP_ABBRV,RTRIM(MSUP_ADDR1) MSUP_ADDR1,MSUP_TELNO,MSUP_FAXNO,RTRIM(MSUP_TAXREG) MSUP_TAXREG");
 		$this->db->like($plike);
 		$query = $this->db->get($this->TABLENAME,10);
 		return $query->result();
