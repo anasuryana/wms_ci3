2908c3af32e163b46b324c304256646416d7550a
Ana <anasuryana10@gmail.com>
2022-11-23 10:01:50 +0700

    Feat : Show PPN Column in Receiving List Report

--- application/controllers/RCVHistory.php
+++ application/controllers/RCVHistory.php
@@ -1,106 +1,114 @@
 <?php
-defined('BASEPATH') OR exit('No direct script access allowed');
+defined('BASEPATH') or exit('No direct script access allowed');
+
 use PhpOffice\PhpSpreadsheet\Spreadsheet;
 use PhpOffice\PhpSpreadsheet\Style\Border;
 use PhpOffice\PhpSpreadsheet\Style\Color;
 use PhpOffice\PhpSpreadsheet\Writer\Xlsx;
-class RCVHistory extends CI_Controller {
 
-	public function __construct()
-	{
-		parent::__construct();		
-		$this->load->model('RCVNI_mod');
+class RCVHistory extends CI_Controller
+{
+
+    public function __construct()
+    {
+        parent::__construct();
+        $this->load->model('RCVNI_mod');
         $this->load->helper('url');
-	}
+    }
 
-	public function index()
-	{
-		echo "sorry";
-	}
+    public function index()
+    {
+        echo "sorry";
+    }
 
-	function form_receiving() {
-		$this->load->view('wms_report/vrpt_receiving_list');
-	}
+    function form_receiving()
+    {
+        $this->load->view('wms_report/vrpt_receiving_list');
+    }
 
-	function receiving() {
-		header('Content-Type: application/json');
-		$searchby = $this->input->get('searchby');
-		$search = $this->input->get('search');
-        $dateFrom = $this->input->get('date0');
-        $dateTo = $this->input->get('date1');
-        $likeFilter = $searchby=='id' ? ['ITEMCODE' => $search] : ['ITEMNAME' => $search];
-		$rs = $this->RCVNI_mod->select_receiving_like($likeFilter,$dateFrom, $dateTo );
-        die(json_encode(['data' => $rs]));
-	}
-
-    function receivingAsXLS() {
+    function receiving()
+    {
+        header('Content-Type: application/json');
         $searchby = $this->input->get('searchby');
-		$search = $this->input->get('search');
+        $search = $this->input->get('search');
         $dateFrom = $this->input->get('date0');
         $dateTo = $this->input->get('date1');
-        $likeFilter = $searchby=='id' ? ['ITEMCODE' => $search] : ['ITEMNAME' => $search];
-		$rs = $this->RCVNI_mod->select_receiving_like($likeFilter,$dateFrom, $dateTo );
+        $likeFilter = $searchby == 'id' ? ['ITEMCODE' => $search] : ['ITEMNAME' => $search];
+        $rs = $this->RCVNI_mod->select_receiving_like($likeFilter, $dateFrom, $dateTo);
+        die(json_encode(['data' => $rs]));
+    }
+
+    function receivingAsXLS()
+    {
+        $searchby = $this->input->get('searchby');
+        $search = $this->input->get('search');
+        $dateFrom = $this->input->get('date0');
+        $dateTo = $this->input->get('date1');
+        $likeFilter = $searchby == 'id' ? ['ITEMCODE' => $search] : ['ITEMNAME' => $search];
+        $rs = $this->RCVNI_mod->select_receiving_like($likeFilter, $dateFrom, $dateTo);
         $spreadsheet = new Spreadsheet();
-		$sheet = $spreadsheet->getActiveSheet();
-		$sheet->setTitle('REPORT');
-        $sheet->setCellValueByColumnAndRow(1,1, 'PT. SMT INDONESIA');
-		$sheet->setCellValueByColumnAndRow(1,2, 'Kawasan EJIP Plot 5C2 Cikarang Selatan');
-		$sheet->setCellValueByColumnAndRow(1,3, 'RECEIVING LIST REPORT');
-		$sheet->setCellValueByColumnAndRow(1,4, 'PERIOD : '.$dateFrom.' - '.$dateTo);
-        $sheet->setCellValueByColumnAndRow(1,5, 'Type BC');
-        $sheet->setCellValueByColumnAndRow(2,5, 'Nomor Pengajuan');
-        $sheet->setCellValueByColumnAndRow(3,5, 'Nomor Pendaftaran');
-        $sheet->setCellValueByColumnAndRow(4,5, 'Tanggal Pendaftaran');
-        $sheet->setCellValueByColumnAndRow(5,5, 'Subject');
-        $sheet->setCellValueByColumnAndRow(6,5, 'Department');
-        $sheet->setCellValueByColumnAndRow(7,5, 'Receiving Number');
-        $sheet->setCellValueByColumnAndRow(8,5, 'Receiving Date');
-        $sheet->setCellValueByColumnAndRow(9,5, 'PO Number');
-        $sheet->setCellValueByColumnAndRow(10,5, 'Vendor Code');
-        $sheet->setCellValueByColumnAndRow(11,5, 'Vendor Name');
-        $sheet->setCellValueByColumnAndRow(12,5, 'Item Code');
-        $sheet->setCellValueByColumnAndRow(13,5, 'Item Name');
-        $sheet->setCellValueByColumnAndRow(14,5, 'Received Qty');
-        $sheet->setCellValueByColumnAndRow(15,5, 'Unit Measurement');
-        $sheet->setCellValueByColumnAndRow(16,5, 'Currency');
-        $sheet->setCellValueByColumnAndRow(17,5, 'Unit Price');
-        $sheet->setCellValueByColumnAndRow(18,5, 'Amount');
+        $sheet = $spreadsheet->getActiveSheet();
+        $sheet->setTitle('REPORT');
+        $sheet->setCellValueByColumnAndRow(1, 1, 'PT. SMT INDONESIA');
+        $sheet->setCellValueByColumnAndRow(1, 2, 'Kawasan EJIP Plot 5C2 Cikarang Selatan');
+        $sheet->setCellValueByColumnAndRow(1, 3, 'RECEIVING LIST REPORT');
+        $sheet->setCellValueByColumnAndRow(1, 4, 'PERIOD : ' . $dateFrom . ' - ' . $dateTo);
+        $sheet->setCellValueByColumnAndRow(1, 5, 'Type BC');
+        $sheet->setCellValueByColumnAndRow(2, 5, 'Nomor Pengajuan');
+        $sheet->setCellValueByColumnAndRow(3, 5, 'Nomor Pendaftaran');
+        $sheet->setCellValueByColumnAndRow(4, 5, 'PPN');
+        $sheet->setCellValueByColumnAndRow(5, 5, 'Tanggal Pendaftaran');
+        $sheet->setCellValueByColumnAndRow(6, 5, 'Subject');
+        $sheet->setCellValueByColumnAndRow(7, 5, 'Department');
+        $sheet->setCellValueByColumnAndRow(8, 5, 'Receiving Number');
+        $sheet->setCellValueByColumnAndRow(9, 5, 'Receiving Date');
+        $sheet->setCellValueByColumnAndRow(10, 5, 'PO Number');
+        $sheet->setCellValueByColumnAndRow(11, 5, 'Vendor Code');
+        $sheet->setCellValueByColumnAndRow(12, 5, 'Vendor Name');
+        $sheet->setCellValueByColumnAndRow(13, 5, 'Item Code');
+        $sheet->setCellValueByColumnAndRow(14, 5, 'Item Name');
+        $sheet->setCellValueByColumnAndRow(15, 5, 'Received Qty');
+        $sheet->setCellValueByColumnAndRow(16, 5, 'Unit Measurement');
+        $sheet->setCellValueByColumnAndRow(17, 5, 'Currency');
+        $sheet->setCellValueByColumnAndRow(18, 5, 'Unit Price');
+        $sheet->setCellValueByColumnAndRow(19, 5, 'Amount');
         $sheet->freezePane('A6');
         $i = 6;
-        foreach($rs as $r) {
-            $sheet->setCellValueByColumnAndRow(1,$i, $r['RCV_BCTYPE']);
-            $sheet->setCellValueByColumnAndRow(2,$i, $r['RCV_RPNO']);
-            $sheet->setCellValueByColumnAndRow(3,$i, $r['RCV_BCNO']);
-            $sheet->setCellValueByColumnAndRow(4,$i, $r['RCV_RPDATE']);
-            $sheet->setCellValueByColumnAndRow(5,$i, $r['POSUBJECT']);
-            $sheet->setCellValueByColumnAndRow(6,$i, $r['PODEPT']);
-            $sheet->setCellValueByColumnAndRow(7,$i, $r['RECEIVINGNO']);
-            $sheet->setCellValueByColumnAndRow(8,$i, $r['RCV_RCVDATE']);
-            $sheet->setCellValueByColumnAndRow(9,$i, $r['RCV_PO']);
-            $sheet->setCellValueByColumnAndRow(10,$i, $r['RCV_SUPCD']);
-            $sheet->setCellValueByColumnAndRow(11,$i, $r['SUPNM']);
-            $sheet->setCellValueByColumnAndRow(12,$i, $r['ITEMCODE']);
-            $sheet->setCellValueByColumnAndRow(13,$i, $r['ITEMNAME']);
-            $sheet->setCellValueByColumnAndRow(14,$i, $r['RQT']);
-            $sheet->setCellValueByColumnAndRow(15,$i, $r['UOM']);
-            $sheet->setCellValueByColumnAndRow(16,$i, $r['MSUP_SUPCR']);
-            $sheet->setCellValueByColumnAndRow(17,$i, $r['RCV_PRPRC']);
-            $sheet->setCellValueByColumnAndRow(18,$i, $r['AMOUNT']);
+        foreach ($rs as $r) {
+            $sheet->setCellValueByColumnAndRow(1, $i, $r['RCV_BCTYPE']);
+            $sheet->setCellValueByColumnAndRow(2, $i, $r['RCV_RPNO']);
+            $sheet->setCellValueByColumnAndRow(3, $i, $r['RCV_BCNO']);
+            $sheet->setCellValueByColumnAndRow(4, $i, $r['PO_VAT']);
+            $sheet->setCellValueByColumnAndRow(5, $i, $r['RCV_RPDATE']);
+            $sheet->setCellValueByColumnAndRow(6, $i, $r['POSUBJECT']);
+            $sheet->setCellValueByColumnAndRow(7, $i, $r['PODEPT']);
+            $sheet->setCellValueByColumnAndRow(8, $i, $r['RECEIVINGNO']);
+            $sheet->setCellValueByColumnAndRow(9, $i, $r['RCV_RCVDATE']);
+            $sheet->setCellValueByColumnAndRow(10, $i, $r['RCV_PO']);
+            $sheet->setCellValueByColumnAndRow(11, $i, $r['RCV_SUPCD']);
+            $sheet->setCellValueByColumnAndRow(12, $i, $r['SUPNM']);
+            $sheet->setCellValueByColumnAndRow(13, $i, $r['ITEMCODE']);
+            $sheet->setCellValueByColumnAndRow(14, $i, $r['ITEMNAME']);
+            $sheet->setCellValueByColumnAndRow(15, $i, $r['RQT']);
+            $sheet->setCellValueByColumnAndRow(16, $i, $r['UOM']);
+            $sheet->setCellValueByColumnAndRow(17, $i, $r['MSUP_SUPCR']);
+            $sheet->setCellValueByColumnAndRow(18, $i, $r['RCV_PRPRC']);
+            $sheet->setCellValueByColumnAndRow(19, $i, $r['AMOUNT']);
             $i++;
         }
-        foreach(range('B', 'R') as $v) {
+        foreach (range('B', 'R') as $v) {
             $sheet->getColumnDimension($v)->setAutoSize(true);
         }
-        $rang = "A5:R".$i;
-			$sheet->getStyle($rang)->getBorders()->getAllBorders()->setBorderStyle(Border::BORDER_THIN)
-			->setColor(new Color('1F1812'));
-        $sheet->getStyle("Q5:R".$i)->getNumberFormat()->setFormatCode('#,##0');
+        $rang = "A5:R" . $i;
+        $sheet->getStyle($rang)->getBorders()->getAllBorders()->setBorderStyle(Border::BORDER_THIN)
+            ->setColor(new Color('1F1812'));
+        $sheet->getStyle("Q5:R" . $i)->getNumberFormat()->setFormatCode('#,##0');
         $writer = new Xlsx($spreadsheet);
         $stringjudul = "Receiving List Report";
-		$filename=$stringjudul; //save our workbook as this file name
-		header('Content-Type: application/vnd.ms-excel');
-		header('Content-Disposition: attachment;filename="'. $filename .'.xlsx"'); 
-		header('Cache-Control: max-age=0');
-		$writer->save('php://output');
+        $filename = $stringjudul; //save our workbook as this file name
+        header('Content-Type: application/vnd.ms-excel');
+        header('Content-Disposition: attachment;filename="' . $filename . '.xlsx"');
+        header('Cache-Control: max-age=0');
+        $writer->save('php://output');
     }
 }
--- application/views/wms_report/vrpt_receiving_list.php
+++ application/views/wms_report/vrpt_receiving_list.php
@@ -1,45 +1,47 @@
 <style>
-    thead tr.first th, thead tr.first td {
+    thead tr.first th,
+    thead tr.first td {
         position: sticky;
-        top: 0;        
+        top: 0;
     }
 
-    thead tr.second th, thead tr.second td {
+    thead tr.second th,
+    thead tr.second td {
         position: sticky;
         top: 25px;
-    }  
+    }
 </style>
 
 <div style="padding: 10px">
-    <div class="container-fluid">        
+    <div class="container-fluid">
         <div class="row" id="rrcvlist_stack1">
             <div class="col-md-3 mb-1">
-                <div class="input-group input-group-sm">                    
-                    <span class="input-group-text" >From</span>                    
+                <div class="input-group input-group-sm">
+                    <span class="input-group-text">From</span>
                     <input type="text" class="form-control" id="rrcvlist_txt_dt" readonly>
                 </div>
             </div>
             <div class="col-md-3 mb-1">
-                <div class="input-group input-group-sm">                    
-                    <span class="input-group-text" >To</span>                    
+                <div class="input-group input-group-sm">
+                    <span class="input-group-text">To</span>
                     <input type="text" class="form-control" id="rrcvlist_txt_dt2" readonly>
                 </div>
             </div>
         </div>
-       
+
         <div class="row" id="rrcvlist_stack2">
-            <div class="col-md-7 mb-1">                       
-                <div class="input-group input-group-sm">                    
-                    <span class="input-group-text" >Search by</span>
+            <div class="col-md-7 mb-1">
+                <div class="input-group input-group-sm">
+                    <span class="input-group-text">Search by</span>
                     <select class="form-select" id="rrcvlist_seachby" onchange="document.getElementById('rrcvlist_txt_search').focus()">
                         <option value="id">Item Code</option>
                         <option value="name">Item Description</option>
                     </select>
                     <input type="text" class="form-control" id="rrcvlist_txt_search">
-                </div>         
+                </div>
             </div>
-            
-            <div class="col-md-3 mb-1">                       
+
+            <div class="col-md-3 mb-1">
                 <div class="btn-group btn-group-sm">
                     <button class="btn btn-primary" type="button" id="rrcvlist_btn_gen" onclick="rrcvlist_btn_gen_eClick(this)">Search</button>
                     <div class="btn-group btn-group-sm" role="group">
@@ -47,15 +49,15 @@
                             Export to
                         </button>
                         <ul class="dropdown-menu" aria-labelledby="btnGroupDrop1">
-                            <li><a class="dropdown-item" href="#" id="rrcvlist_btn_xls" onclick="rrcvlist_btn_xls_eClick(this)"><span style="color: MediumSeaGreen"><i class="fas fa-file-excel"></i></span> XLS</a></li>                            
+                            <li><a class="dropdown-item" href="#" id="rrcvlist_btn_xls" onclick="rrcvlist_btn_xls_eClick(this)"><span style="color: MediumSeaGreen"><i class="fas fa-file-excel"></i></span> XLS</a></li>
                         </ul>
-                    </div>                   
+                    </div>
                 </div>
             </div>
             <div class="col-md-2 mb-1 text-end">
-                <span id="rrcvlist_lblinfo" class="badge bg-info"></span>               
+                <span id="rrcvlist_lblinfo" class="badge bg-info"></span>
             </div>
-        </div>        
+        </div>
         <div class="row">
             <div class="col-md-12 mb-1">
                 <div class="table-responsive" id="rrcvlist_divku">
@@ -80,31 +82,32 @@
                                 <th class="text-center">Currency</th>
                                 <th class="text-end">Unit Price</th>
                                 <th class="text-end">Amount</th>
-                            </tr>                            
+                            </tr>
                         </thead>
-                        <tbody>                        
+                        <tbody>
                         </tbody>
                     </table>
                 </div>
             </div>
-        </div>           	
+        </div>
     </div>
 </div>
 <script>
-    $("#rrcvlist_divku").css('height', $(window).height()   
-    -document.getElementById('rrcvlist_stack1').offsetHeight 
-    -document.getElementById('rrcvlist_stack2').offsetHeight    
-    -100)
+    $("#rrcvlist_divku").css('height', $(window).height() -
+        document.getElementById('rrcvlist_stack1').offsetHeight -
+        document.getElementById('rrcvlist_stack2').offsetHeight -
+        100)
     $("#rrcvlist_txt_dt").datepicker({
         format: 'yyyy-mm-dd',
-        autoclose:true
+        autoclose: true
     });
     $("#rrcvlist_txt_dt2").datepicker({
         format: 'yyyy-mm-dd',
-        autoclose:true
+        autoclose: true
     });
     $("#rrcvlist_txt_dt").datepicker('update', new Date());
     $("#rrcvlist_txt_dt2").datepicker('update', new Date());
+
     function rrcvlist_btn_gen_eClick(p) {
         p.disabled = true
         const searchby = document.getElementById('rrcvlist_seachby').value
@@ -113,10 +116,15 @@
         const date1 = document.getElementById('rrcvlist_txt_dt2').value
         $.ajax({
             type: "GET",
-            url: "<?=base_url('RCVHistory/receiving')?>",
-            data: {searchby: searchby, search:search, date0: date0, date1:date1 },
+            url: "<?= base_url('RCVHistory/receiving') ?>",
+            data: {
+                searchby: searchby,
+                search: search,
+                date0: date0,
+                date1: date1
+            },
             dataType: "json",
-            success: function (response) {
+            success: function(response) {
                 p.disabled = false
                 let ttlrows = response.data.length;
                 let mydes = document.getElementById("rrcvlist_divku");
@@ -127,8 +135,8 @@
                 let tabell = myfrag.getElementById("rrcvlist_tbl")
                 let tableku2 = tabell.getElementsByTagName("tbody")[0]
                 let newrow, newcell
-                tableku2.innerHTML='';
-                for (let i = 0; i<ttlrows; i++){
+                tableku2.innerHTML = '';
+                for (let i = 0; i < ttlrows; i++) {
                     newrow = tableku2.insertRow(-1)
                     newcell = newrow.insertCell(0)
                     newcell.innerHTML = response.data[i].RCV_BCTYPE
@@ -137,42 +145,46 @@
                     newcell = newrow.insertCell(2)
                     newcell.innerHTML = response.data[i].RCV_BCNO
                     newcell = newrow.insertCell(3)
-                    newcell.innerHTML = response.data[i].RCV_RPDATE
+                    newcell.innerHTML = response.data[i].PO_VAT
+                    newcell.classList.add('text-end')
                     newcell = newrow.insertCell(4)
-                    newcell.innerHTML = response.data[i].POSUBJECT
+                    newcell.innerHTML = response.data[i].RCV_RPDATE
                     newcell = newrow.insertCell(5)
-                    newcell.innerHTML = response.data[i].PODEPT
+                    newcell.innerHTML = response.data[i].POSUBJECT
                     newcell = newrow.insertCell(6)
-                    newcell.innerHTML = response.data[i].RECEIVINGNO
+                    newcell.innerHTML = response.data[i].PODEPT
                     newcell = newrow.insertCell(7)
-                    newcell.innerHTML = response.data[i].RCV_RCVDATE
+                    newcell.innerHTML = response.data[i].RECEIVINGNO
                     newcell = newrow.insertCell(8)
-                    newcell.innerHTML = response.data[i].RCV_PO
+                    newcell.innerHTML = response.data[i].RCV_RCVDATE
                     newcell = newrow.insertCell(9)
-                    newcell.innerHTML = response.data[i].RCV_SUPCD
+                    newcell.innerHTML = response.data[i].RCV_PO
                     newcell = newrow.insertCell(10)
-                    newcell.innerHTML = response.data[i].SUPNM
+                    newcell.innerHTML = response.data[i].RCV_SUPCD
                     newcell = newrow.insertCell(11)
-                    newcell.innerHTML = response.data[i].ITEMCODE
+                    newcell.innerHTML = response.data[i].SUPNM
                     newcell = newrow.insertCell(12)
-                    newcell.innerHTML = response.data[i].ITEMNAME
+                    newcell.innerHTML = response.data[i].ITEMCODE
                     newcell = newrow.insertCell(13)
+                    newcell.innerHTML = response.data[i].ITEMNAME
+                    newcell = newrow.insertCell(14)
                     newcell.classList.add('text-end')
                     newcell.innerHTML = numeral(response.data[i].RQT).format(',')
-                    newcell = newrow.insertCell(14)
-                    newcell.innerHTML = response.data[i].UOM
                     newcell = newrow.insertCell(15)
-                    newcell.innerHTML = response.data[i].MSUP_SUPCR
+                    newcell.innerHTML = response.data[i].UOM
                     newcell = newrow.insertCell(16)
+                    newcell.innerHTML = response.data[i].MSUP_SUPCR
+                    newcell = newrow.insertCell(17)
                     newcell.classList.add('text-end')
                     newcell.innerHTML = numeral(response.data[i].RCV_PRPRC).format(',')
-                    newcell = newrow.insertCell(17)
+                    newcell = newrow.insertCell(18)
                     newcell.classList.add('text-end')
                     newcell.innerHTML = numeral(response.data[i].AMOUNT).format(',')
                 }
-                mydes.innerHTML='';
+                mydes.innerHTML = '';
                 mydes.appendChild(myfrag);
-            }, error: function(xhr, xopt, xthrow){
+            },
+            error: function(xhr, xopt, xthrow) {
                 alertify.error(xthrow)
                 p.disabled = false
             }
@@ -187,20 +199,27 @@
         const search = document.getElementById('rrcvlist_txt_search').value
         const date0 = document.getElementById('rrcvlist_txt_dt').value
         const date1 = document.getElementById('rrcvlist_txt_dt2').value
-        $.ajax({            
-            url: "<?=base_url('RCVHistory/receivingAsXLS')?>",
-            data: {searchby: searchby, search:search, date0: date0, date1:date1 },
-            success: function (response) {
-                const blob = new Blob([response], { type: "application/vnd.ms-excel" })
-                const fileName =  `Receiving List Report ${date0} - ${date1}.xlsx`
+        $.ajax({
+            url: "<?= base_url('RCVHistory/receivingAsXLS') ?>",
+            data: {
+                searchby: searchby,
+                search: search,
+                date0: date0,
+                date1: date1
+            },
+            success: function(response) {
+                const blob = new Blob([response], {
+                    type: "application/vnd.ms-excel"
+                })
+                const fileName = `Receiving List Report ${date0} - ${date1}.xlsx`
                 saveAs(blob, fileName)
                 p.classList.remove('disabled')
                 alertify.success('Done')
                 infoDiv.innerHTML = ''
             },
-            xhr: function () {
+            xhr: function() {
                 const xhr = new XMLHttpRequest()
-                xhr.onreadystatechange = function () {
+                xhr.onreadystatechange = function() {
                     if (xhr.readyState == 2) {
                         if (xhr.status == 200) {
                             xhr.responseType = "blob";
