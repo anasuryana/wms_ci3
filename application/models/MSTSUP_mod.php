41ad2de8898db7be45ea67c810b162886ce8de55
Ana <anasuryana10@gmail.com>
2023-01-05 09:43:56 +0700

    refactoring

--- application/models/MSTSUP_mod.php
+++ application/models/MSTSUP_mod.php
@@ -1,77 +1,82 @@
 <?php
 
-class MSTSUP_mod extends CI_Model {
-	private $TABLENAME = "MSUP_TBL";
-	public function __construct()
+class MSTSUP_mod extends CI_Model
+{
+    private $TABLENAME = "MSUP_TBL";
+    public function __construct()
     {
         $this->load->database();
-	}
-	public function check_Primary($data)
-    {        
-        return $this->db->get_where($this->TABLENAME,$data)->num_rows();
-	}
-	public function updatebyId($pdata, $pkey)
-    {        
-        $this->db->where('MSUP_SUPCD',$pkey);
+    }
+    public function check_Primary($data)
+    {
+        return $this->db->get_where($this->TABLENAME, $data)->num_rows();
+    }
+    public function updatebyId($pdata, $pkey)
+    {
+        $this->db->where('MSUP_SUPCD', $pkey);
         $this->db->update($this->TABLENAME, $pdata);
         return $this->db->affected_rows();
     }
-	public function update_where($pdata, $pwhere)
-    {        
+    public function update_where($pdata, $pwhere)
+    {
         $this->db->where($pwhere);
         $this->db->update($this->TABLENAME, $pdata);
         return $this->db->affected_rows();
     }
-	public function selectAll()
-	{
-		$this->db->select("MSUP_SUPCD,MSUP_SUPCR,MSUP_SUPNM,MSUP_ABBRV,MSUP_ADDR1");
-		$query = $this->db->get($this->TABLENAME);
-		return $query->result();
-	}
-	public function select3()
-	{
-		$this->db->select("MSUP_SUPCD,MSUP_SUPCR,MSUP_SUPNM");		
-		$query = $this->db->get($this->TABLENAME);
-		return $query->result();
-	}
+    public function selectAll()
+    {
+        $this->db->select("MSUP_SUPCD,MSUP_SUPCR,MSUP_SUPNM,MSUP_ABBRV,MSUP_ADDR1");
+        $query = $this->db->get($this->TABLENAME);
+        return $query->result();
+    }
+    public function select3()
+    {
+        $this->db->select("MSUP_SUPCD,MSUP_SUPCR,MSUP_SUPNM");
+        $query = $this->db->get($this->TABLENAME);
+        return $query->result();
+    }
 
-	public function select_like($plike){
-		$this->db->select("MSUP_SUPCD,MSUP_SUPCR,MSUP_SUPNM,MSUP_ABBRV,RTRIM(MSUP_ADDR1) MSUP_ADDR1,MSUP_TELNO,MSUP_FAXNO,RTRIM(MSUP_TAXREG) MSUP_TAXREG,RTRIM(MSUP_PTERM) MSUP_PTERM");
-		$this->db->like($plike);
-		$query = $this->db->get($this->TABLENAME,10);
-		return $query->result();
-	}
+    public function select_like($plike)
+    {
+        $this->db->select("MSUP_SUPCD,MSUP_SUPCR,MSUP_SUPNM,MSUP_ABBRV,RTRIM(MSUP_ADDR1) MSUP_ADDR1,MSUP_TELNO,MSUP_FAXNO,RTRIM(MSUP_TAXREG) MSUP_TAXREG,RTRIM(MSUP_PTERM) MSUP_PTERM");
+        $this->db->like($plike);
+        $query = $this->db->get($this->TABLENAME, 10);
+        return $query->result();
+    }
 
-	public function selectbyName($pname)
-	{
-		$this->db->select("MSUP_SUPCD,MSUP_SUPCR,MSUP_SUPNM");
-		$this->db->like('lower(MSUP_SUPNM)', $pname);
-		$query = $this->db->get($this->TABLENAME, 10);
-		return $query->result();
-	}
-	
-	public function insert($data)
-    {        
-        $this->db->insert($this->TABLENAME,$data);
+    public function selectbyName($pname)
+    {
+        $this->db->select("MSUP_SUPCD,MSUP_SUPCR,MSUP_SUPNM");
+        $this->db->like('lower(MSUP_SUPNM)', $pname);
+        $query = $this->db->get($this->TABLENAME, 10);
+        return $query->result();
+    }
+
+    public function insert($data)
+    {
+        $this->db->insert($this->TABLENAME, $data);
         return $this->db->affected_rows();
     }
-	
-	public function select_union($plike){
-		$this->db->select("MSUP_SUPCD,MSUP_SUPNM,MSUP_SUPCR");
-		$this->db->like($plike);
-		$query = $this->db->get("v_supplier_customer_union",100);
-		return $query->result();
-	}
 
-	function select_where_id_in($arrayCD) {
-		$this->db->select("RTRIM(MSUP_SUPCD) SUPCD,RTRIM(MSUP_SUPNM) SUPNM,MSUP_SUPCR");
-		$this->db->where_in("MSUP_SUPCD", $arrayCD);
-		$query = $this->db->get($this->TABLENAME);
-		return $query->result_array();
-	}
+    public function select_union($plike)
+    {
+        $this->db->select("MSUP_SUPCD,MSUP_SUPNM,MSUP_SUPCR");
+        $this->db->like($plike);
+        $query = $this->db->get("v_supplier_customer_union", 100);
+        return $query->result();
+    }
 
-	public function sync(){
-		$qry = "
+    public function select_where_id_in($arrayCD)
+    {
+        $this->db->select("RTRIM(MSUP_SUPCD) SUPCD,RTRIM(MSUP_SUPNM) SUPNM,MSUP_SUPCR");
+        $this->db->where_in("MSUP_SUPCD", $arrayCD);
+        $query = $this->db->get($this->TABLENAME);
+        return $query->result_array();
+    }
+
+    public function sync()
+    {
+        $qry = "
 		INSERT INTO MSUP_TBL (
 		MSUP_SUPCD,
 		MSUP_SUPCR,
@@ -121,16 +126,17 @@ class MSTSUP_mod extends CI_Model {
 		)
 		select m.* from XMSUP m left join MSUP_TBL w on m.MSUP_SUPCD=w.MSUP_SUPCD
 		where w.MSUP_SUPCD is null";
-		$this->db->query($qry);
-		return $this->db->affected_rows();  		
-	}
+        $this->db->query($qry);
+        return $this->db->affected_rows();
+    }
 
-	public function update_based_on_parent() {
-		$qry = "UPDATE A
+    public function update_based_on_parent()
+    {
+        $qry = "UPDATE A
 		SET A.MSUP_ADDR1=B.MSUP_ADDR1,A.MSUP_ADDR2=B.MSUP_ADDR2,A.MSUP_TELNO=B.MSUP_TELNO,A.MSUP_PIC=B.MSUP_PIC
 		from MSUP_TBL A
 		left join XMSUP B on A.MSUP_SUPCD=B.MSUP_SUPCD";
-		$this->db->query($qry);
-		return $this->db->affected_rows();
-	}
+        $this->db->query($qry);
+        return $this->db->affected_rows();
+    }
 }
