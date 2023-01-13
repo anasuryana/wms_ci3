3e0a566bebd9c01a0fb0ffb873fbdd06b40130bb
Ana <anasuryana10@gmail.com>
2023-01-13 14:33:20 +0700

    refactoring

--- application/views/wms/vcombinelabel2.php
+++ application/views/wms/vcombinelabel2.php
@@ -2,12 +2,12 @@
 	<div class="container-xxl">
         <div class="row">
             <div class="col-md-12 mb-1">
-                <div class="input-group input-group-sm mb-1">                    
+                <div class="input-group input-group-sm mb-1">
                     <label class="input-group-text">Reff Code</label>
-                    <input type="text" class="form-control" id="combinelbl2_reffcd" placeholder="Scan or type reff number here" maxlength="200">                 
+                    <input type="text" class="form-control" id="combinelbl2_reffcd" placeholder="Scan or type reff number here" maxlength="200">
                 </div>
-            </div>            
-        </div> 
+            </div>
+        </div>
         <div class="row">
             <div class="col-md-12 mb-1">
                 <span id="combinelbl2_lblinfo" class="badge bg-info"></span>
@@ -19,14 +19,14 @@
                     <table id="combinelbl2_tbl" class="table table-sm table-striped table-bordered table-hover" style="width:100%;cursor:pointer">
                         <thead class="table-light">
                             <tr>
-                                
-                                <th  class="align-middle">Reff No</th>                                
+
+                                <th  class="align-middle">Reff No</th>
                                 <th  class="align-middle">Job Number</th>
                                 <th  class="align-middle">Item Code</th>
                                 <th  class="align-middle">Item Name</th>
-                                <th  class="text-right">QTY</th>                                                           
+                                <th  class="text-right">QTY</th>
                                 <th  class="d-none">QTY /Sheet</th>
-                            </tr>                          
+                            </tr>
                         </thead>
                         <tbody>
                         </tbody>
@@ -36,9 +36,9 @@
         </div>
         <div class="row">
             <div class="col ">
-                <div class="input-group input-group-sm mb-1">                    
-                    <label class="input-group-text">New Reff Code</label>                    
-                    <input type="text" class="form-control" id="combinelbl2_reffcdnew"  readonly title="it will show You new reff no, triggered when You save desired labels">                     
+                <div class="input-group input-group-sm mb-1">
+                    <label class="input-group-text">New Reff Code</label>
+                    <input type="text" class="form-control" id="combinelbl2_reffcdnew"  readonly title="it will show You new reff no, triggered when You save desired labels">
                     <button class="btn btn-primary" id="combinelbl2_btnmod"><i class="fas fa-search"></i></button>
                 </div>
             </div>
@@ -56,34 +56,27 @@
 </div>
 <div class="modal fade" id="combinelbl2_MOD">
     <div class="modal-dialog modal-xl">
-      <div class="modal-content">      
+      <div class="modal-content">
         <!-- Modal Header -->
         <div class="modal-header">
             <h4 class="modal-title">List</h4>
             <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
         </div>
-        
+
         <!-- Modal body -->
         <div class="modal-body">
             <div class="row">
                 <div class="col">
-                    <div class="input-group input-group-sm mb-1">                        
-                        <span class="input-group-text" >Search</span>                        
+                    <div class="input-group input-group-sm mb-1">
+                        <span class="input-group-text" >Search</span>
+                        <select id="combinelbl2_srchby" onchange="combinelbl2_e_fokus()" class="form-select">
+                            <option value="1">New Reff</option>
+                            <option value="2">Old Reff</option>
+                        </select>
                         <input type="text" class="form-control" id="combinelbl2_txtsearch" onkeypress="combinelbl2_e_search(event)" maxlength="16" onfocus="this.select()" required placeholder="...">
                     </div>
                 </div>
             </div>
-            <div class="row">
-                <div class="col">
-                    <div class="input-group input-group-sm mb-1">                        
-                        <span class="input-group-text" >Search by</span>                        
-                        <select id="combinelbl2_srchby" onchange="combinelbl2_e_fokus()" class="form-select">
-                            <option value="1">New Reff</option>
-                            <option value="2">Old Reff</option>
-                        </select>                  
-                    </div>
-                </div>
-            </div>            
             <div class="row">
                 <div class="col mb-1 text-right">
                     <span class="badge bg-info" id="combinelbl2_lblinfo_mod"></span>
@@ -97,7 +90,7 @@
                                 <tr>
                                     <th>New Reff</th>
                                     <th>Old Reff</th>
-                                    <th>QTY</th>                                    
+                                    <th>QTY</th>
                                 </tr>
                             </thead>
                             <tbody>
@@ -106,7 +99,7 @@
                     </div>
                 </div>
             </div>
-        </div>             
+        </div>
       </div>
     </div>
 </div>
@@ -115,14 +108,13 @@
         document.getElementById('combinelbl2_txtsearch').focus();
     }
     $("#combinelbl2_divku").css('height', $(window).height()*67/100);
-    $("#combinelbl2_btnmod").click(function (e) { 
+    $("#combinelbl2_btnmod").click(function (e) {
         $("#combinelbl2_MOD").modal('show');
-        
     });
     $("#combinelbl2_MOD").on('shown.bs.modal', function(){
         document.getElementById('combinelbl2_txtsearch').focus();
     });
-    $("#combinelbl2_txtsearch").keypress(function (e) { 
+    $("#combinelbl2_txtsearch").keypress(function (e) {
         if(e.which==13){
             let msearch = document.getElementById('combinelbl2_txtsearch').value;
             let msearch_by = document.getElementById('combinelbl2_srchby').value;
@@ -132,7 +124,7 @@
                 url: "<?=base_url('SER/combined2_list')?>",
                 data: {insearch: msearch, insearch_by: msearch_by},
                 dataType: "json",
-                success: function (response) {                    
+                success: function (response) {
                     if(response.status[0].cd!='0'){
                         document.getElementById('combinelbl2_lblinfo_mod').innerText = '' ;
                         let ttlrows = response.data.length;
@@ -145,16 +137,16 @@
                         let tableku2 = tabell.getElementsByTagName("tbody")[0];
                         let newrow, newcell, newText;
                         tableku2.innerHTML='';
-                        for (let i = 0; i<ttlrows; i++){                    
+                        for (let i = 0; i<ttlrows; i++){
                             newrow = tableku2.insertRow(-1);
-                            newcell = newrow.insertCell(0);            
-                            newText = document.createTextNode(response.data[i].SERC_NEWID);            
+                            newcell = newrow.insertCell(0);
+                            newText = document.createTextNode(response.data[i].SERC_NEWID);
                             newcell.appendChild(newText);
-                            newcell = newrow.insertCell(1);            
-                            newText = document.createTextNode(response.data[i].SERC_COMID);            
+                            newcell = newrow.insertCell(1);
+                            newText = document.createTextNode(response.data[i].SERC_COMID);
                             newcell.appendChild(newText);
-                            newcell = newrow.insertCell(2);            
-                            newText = document.createTextNode(response.data[i].SERC_COMQTY);            
+                            newcell = newrow.insertCell(2);
+                            newText = document.createTextNode(response.data[i].SERC_COMQTY);
                             newcell.appendChild(newText);
                         }
                         let mrows = tableku2.getElementsByTagName("tr");
@@ -166,10 +158,10 @@
                         for(let x=0;x<mrows.length;x++){
                             tableku2.rows[x].cells[0].onclick = function(){combine2_getselected(tableku2.rows[x])};
                         }
-                        mydes.innerHTML='';                            
+                        mydes.innerHTML='';
                         mydes.appendChild(myfrag);
                     } else {
-                        document.getElementById('combinelbl2_lblinfo_mod').innerText =response.status[0].msg;                        
+                        document.getElementById('combinelbl2_lblinfo_mod').innerText =response.status[0].msg;
                     }
                 }, error: function(xhr, xopt, xthrow){
                     alertify.error(xthrow);
@@ -177,7 +169,7 @@
             });
         }
     });
-    $("#combinelbl2_btn_print").click(function (e) { 
+    $("#combinelbl2_btn_print").click(function (e) {
         let mreff  = document.getElementById('combinelbl2_reffcdnew').value;
         if(mreff.trim()==''){
             document.getElementById('combinelbl2_reffcdnew').focus();
@@ -185,18 +177,18 @@
             return;
         }
         let serlist = [];
-        serlist.push(mreff); 
-        
+        serlist.push(mreff);
+
         Cookies.set('PRINTLABEL_FG', serlist, {expires:365});
         Cookies.set('PRINTLABEL_FG_LBLTYPE', '1', {expires:365});
         Cookies.set('PRINTLABEL_FG_SIZE', 'A4', {expires:365});
         window.open("<?=base_url('printlabel_fg')?>",'_blank');
     });
-    $("#combinelbl2_btn_save").click(function (e) { 
+    $("#combinelbl2_btn_save").click(function (e) {
         let mtbl = document.getElementById('combinelbl2_tbl');
         let mtbltr = mtbl.getElementsByTagName('tr');
         let ttlrows = mtbltr.length;
-        
+
         if(ttlrows>1){
             let konfirm = confirm('Are You sure ?');
             if(konfirm){
@@ -255,7 +247,7 @@
                 let mtbl = document.getElementById('combinelbl2_tbl');
                 let mtbltr = mtbl.getElementsByTagName('tr');
                 let ttlrows = mtbltr.length;
-                for(let i=0;i<ttlrows;i++){                    
+                for(let i=0;i<ttlrows;i++){
                     if(mtbl.rows[i].cells[0].innerText==mreff){
                         alertify.warning('the label is already in the list below');
                         document.getElementById('combinelbl2_reffcd').value='';
@@ -278,21 +270,21 @@
                         document.getElementById('combinelbl2_reffcd').readOnly=false;
                         document.getElementById('combinelbl2_reffcd').value='';
                         if(response.status[0].cd!='0'){
-                            document.getElementById('combinelbl2_reffcd').value='';                            
+                            document.getElementById('combinelbl2_reffcd').value='';
                             let mtbl = document.getElementById('combinelbl2_tbl');
                             let tableku2 = mtbl.getElementsByTagName("tbody")[0];
                             let mtbltr = mtbl.getElementsByTagName('tr');
                             let ttlrows = response.data.length;
                             let ttlrowsTEMP = mtbltr.length;
                             let newrow, newcell, newText;
-                            for(let i=1;i<ttlrowsTEMP;i++){                    
+                            for(let i=1;i<ttlrowsTEMP;i++){
                                 if(mtbl.rows[i].cells[2].innerText.trim()!=response.data[0].SER_ITMID.trim()){
-                                    alertify.warning('Currently, system only allow to combine same Assy Code '); 
+                                    alertify.warning('Currently, system only allow to combine same Assy Code ');
                                     return;
                                 }
                             }
                             for(let i=0;i<ttlrows;i++){
-                                newrow = tableku2.insertRow(-1);                                                                
+                                newrow = tableku2.insertRow(-1);
                                 newcell = newrow.insertCell(0);
                                 newText = document.createTextNode(response.data[i].SER_ID);
                                 newcell.appendChild(newText);
@@ -317,7 +309,7 @@
                         } else {
                             alertify.warning(response.status[0].msg);
                         }
-                        
+
                     }, error: function(xhr, xopt, xthrow){
                         alertify.error(xthrow);
                     }
@@ -328,12 +320,13 @@
             }
         }
     });
-    $("#combinelbl2_btnnew").click(function (e) { 
+    $("#combinelbl2_btnnew").click(function (e) {
         document.getElementById('combinelbl2_reffcd').value='';
         document.getElementById('combinelbl2_reffcd').readOnly=false;
         document.getElementById('combinelbl2_btn_save').disabled=false;
         document.getElementById('combinelbl2_reffcd').focus();
-        $("#combinelbl2_tbl tbody").empty();        
+        $("#combinelbl2_tbl tbody").empty();
+        document.getElementById('combinelbl2_reffcdnew').value='';
     });
-    
+
 </script>
