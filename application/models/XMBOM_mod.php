d0e8024b8c7b9ed13c2a3f06a2aeae1c28203a1b
Ana <anasuryana10@gmail.com>
2023-04-12 14:26:05 +0700

    Feat : function get version

--- application/models/XMBOM_mod.php
+++ application/models/XMBOM_mod.php
@@ -1,20 +1,32 @@
-<?php 
-class XMBOM_mod extends CI_Model {
-	private $TABLENAME = "XMBOM";
-	public function __construct()
+<?php
+class XMBOM_mod extends CI_Model
+{
+    private $TABLENAME = "XMBOM";
+    public function __construct()
     {
         $this->load->database();
-    }		
-
-    public function selectall(){
-        $this->db->from($this->TABLENAME);        
-		$query = $this->db->get();
-		return $query->result();
     }
 
-    function select_for_simulation($assyCode, $qty) {
+    public function selectall()
+    {
+        $this->db->from($this->TABLENAME);
+        $query = $this->db->get();
+        return $query->result();
+    }
+    public function selectVersionWhereItemIn($model)
+    {
+        $this->db->select("RTRIM(MBOM_MDLCD) MBOM_MDLCD,MAX(MBOM_BOMRV) MBOM_BOMRV");
+        $this->db->where_in("MBOM_MDLCD", $model);
+        $this->db->from($this->TABLENAME);
+        $this->db->group_by("MBOM_MDLCD");
+        $query = $this->db->get();
+        return $query->result_array();
+    }
+
+    public function select_for_simulation($assyCode, $qty)
+    {
         $qry = "select CONCAT('BOM-RV-',MBOM_BOMRV) SIMNO
-        , RTRIM(MBOM_MDLCD) PDPP_MDLCD 
+        , RTRIM(MBOM_MDLCD) PDPP_MDLCD
         , '-' PIS3_WONO
         , MBOM_PARQT*$qty REQQTY
         , MBOM_PARQT MYPER
@@ -28,13 +40,14 @@ class XMBOM_mod extends CI_Model {
         where MBOM_MDLCD=?
         AND MBOM_BOMRV=(SELECT MAX(MBOM_BOMRV) FROM XMBOM WHERE MBOM_MDLCD=?)
         order by MBOM_BOMPN";
-        $query = $this->db->query($qry, [$assyCode, $assyCode] );
+        $query = $this->db->query($qry, [$assyCode, $assyCode]);
         return $query->result_array();
     }
 
-    function select_sim_item_stock($assyCode) {
+    public function select_sim_item_stock($assyCode)
+    {
         $qry = "select PIS3_MPART ITEMCODE,ISNULL(TSTKQTY,0) TSTKQTY from
-        (select RTRIM(MBOM_BOMPN) PIS3_MPART from XMBOM            
+        (select RTRIM(MBOM_BOMPN) PIS3_MPART from XMBOM
             where MBOM_MDLCD=?
             AND MBOM_BOMRV=(SELECT MAX(MBOM_BOMRV) FROM XMBOM WHERE MBOM_MDLCD=?)
             GROUP BY MBOM_BOMPN
@@ -45,7 +58,7 @@ class XMBOM_mod extends CI_Model {
         WHERE  ITH_WH IN ('ARWH1')
         group by
         ITH_ITMCD,ITH_WH) vitemstock group by ITH_ITMCD) vitemstockgroup on PIS3_MPART=ITH_ITMCD";
-        $query = $this->db->query($qry, [$assyCode, $assyCode] );
+        $query = $this->db->query($qry, [$assyCode, $assyCode]);
         return $query->result_array();
     }
 }
