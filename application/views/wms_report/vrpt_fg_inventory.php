c6abe88e8c6306e76c5641faec4b2d4859671c4a
Ana <anasuryana10@gmail.com>
2023-04-30 11:25:48 +0700

    Fix : UI UX

--- application/views/wms_report/vrpt_fg_inventory.php
+++ application/views/wms_report/vrpt_fg_inventory.php
@@ -1,11 +1,22 @@
+<style>    
+    thead tr.first th, thead tr.first td {
+        position: sticky;
+        top: 0;        
+    }
+
+    thead tr.second th, thead tr.second td {
+        position: sticky;
+        top: 26px;
+    }
+</style>
 <div style="padding: 10px">
-    <div class="container-fluid">                     
-        <div class="row">            
-            <div class="col-md-12 mb-1 text-center">                       
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-md-12 mb-1 text-center">
                 <div class="btn-group btn-group-sm">
-                    <button class="btn btn-primary" type="button" id="fginventory_btn_gen"><i class="fas fa-sync"></i></button>                    
+                    <button class="btn btn-primary" type="button" id="fginventory_btn_gen"><i class="fas fa-sync"></i></button>
                     <button class="btn btn-danger" type="button" id="fginventory_btn_trash"><i class="fas fa-trash"></i></button>
-                </div>                
+                </div>
             </div>
         </div>
         <div class="row">
@@ -18,7 +29,7 @@
                 <div class="table-responsive" id="fginventory_divku">
                     <table id="fginventory_tbl" class="table table-striped table-bordered table-sm table-hover" style="font-size:85%">
                         <thead class="table-light">
-                            <tr>
+                            <tr class="first">
                                 <th  class="align-middle">Assy Number</th>
                                 <th  class="align-middle">Model</th>
                                 <th  class="align-middle">Lot</th>
@@ -26,31 +37,31 @@
                                 <th  class="text-end">Qty</th>
                                 <th  class="text-center">Reff. Number</th>
                                 <th  class="text-center">PIC</th>
-                                <th  class="text-center">Time</th>                                
-                                <th  class="text-center">Location</th>                           
-                            </tr>                           
+                                <th  class="text-center">Time</th>
+                                <th  class="text-center">Location</th>
+                            </tr>
                         </thead>
-                        <tbody>                        
+                        <tbody>
                         </tbody>
                     </table>
                 </div>
             </div>
-        </div>    
+        </div>
         <div class="row">
             <div class="col-md-6 mb-1 text-end">
             </div>
             <div class="col-md-6 mb-1 text-end">
-                <div class="input-group input-group-sm">                    
-                    <span class="input-group-text" >TOTAL</span>                    
+                <div class="input-group input-group-sm">
+                    <span class="input-group-text" >TOTAL</span>
                     <input type="text" class="form-control" id="fginventory_txt_total" readonly>
                 </div>
             </div>
-        </div>   	
+        </div>
     </div>
 </div>
 <script>
     $("#fginventory_divku").css('height', $(window).height()*72/100);
-    $("#fginventory_btn_trash").click(function (e) { 
+    $("#fginventory_btn_trash").click(function (e) {
         if(confirm("Are You sure ?")){
             let pw = prompt("PIN");
             $.ajax({
@@ -69,77 +80,75 @@
                     alertify.error(xthrow);
                 }
             });
-        }        
+        }
     });
-    $("#fginventory_btn_gen").click(function (e) {         
-        document.getElementById('fginventory_lblinfo').innerHTML = 'Please wait... <i class="fas fa-spinner fa-spin"></i>';
+    $("#fginventory_btn_gen").click(function (e) {
+        fginventory_btn_gen.innerHTML = 'Please wait...'
+        fginventory_btn_gen.disabled = true
         $.ajax({
             type: "get",
-            url: "<?=base_url('Inventory/getlist')?>",            
+            url: "<?=base_url('Inventory/getlist')?>",
             dataType: "json",
             success: function (response) {
+                fginventory_btn_gen.innerHTML = `<i class="fas fa-sync"></i>`
+                fginventory_btn_gen.disabled = false
                 let ttlrows = response.data.length;
                 let mydes = document.getElementById("fginventory_divku");
                 let myfrag = document.createDocumentFragment();
                 let mtabel = document.getElementById("fginventory_tbl");
                 let cln = mtabel.cloneNode(true);
-                myfrag.appendChild(cln);                    
-                let tabell = myfrag.getElementById("fginventory_tbl");                    
-                let tableku2 = tabell.getElementsByTagName("tbody")[0];//document.getElementById("rprod_tblwo").getElementsByTagName("tbody")[0];
+                myfrag.appendChild(cln);
+                let tabell = myfrag.getElementById("fginventory_tbl");
+                let tableku2 = tabell.getElementsByTagName("tbody")[0]
                 let newrow, newcell, newText;
                 tableku2.innerHTML='';
                 let tmpnomor = '';
                 let mnomor =0;
-                let ttlqty = 0;                
+                let ttlqty = 0;
                 for (let i = 0; i<ttlrows; i++){
                     ttlqty += numeral(response.data[i].CQTY).value();
                     newrow = tableku2.insertRow(-1);
-                    newcell = newrow.insertCell(0);            
-                    newText = document.createTextNode(response.data[i].CASSYNO);
-                    newcell.appendChild(newText);
+                    newcell = newrow.insertCell(0);
+                    newcell.innerHTML = response.data[i].CASSYNO;
+
                     newcell = newrow.insertCell(1);
-                    newText = document.createTextNode(response.data[i].CMODEL);
-                    newcell.appendChild(newText);
-                    
+                    newcell.innerHTML = response.data[i].CMODEL
+
                     newcell = newrow.insertCell(2);
-                    newText = document.createTextNode(response.data[i].CLOTNO);
                     newcell.style.cssText= "white-space: nowrap";
-                    newcell.appendChild(newText);
+                    newcell.innerHTML = response.data[i].CLOTNO
+
                     newcell = newrow.insertCell(3);
-                    newText = document.createTextNode(response.data[i].SER_DOC);
                     newcell.style.cssText= "white-space: nowrap";
-                    newcell.appendChild(newText);
+                    newcell.innerHTML = response.data[i].SER_DOC
 
                     newcell = newrow.insertCell(4);
-                    newText = document.createTextNode(numeral(response.data[i].CQTY).format('0,0'));
                     newcell.style.cssText= "white-space: nowrap;text-align:right";
-                    newcell.appendChild(newText);
+                    newcell.innerHTML = numeral(response.data[i].CQTY).format('0,0')
 
                     newcell = newrow.insertCell(5);
-                    newText = document.createTextNode(response.data[i].REFNO);
                     newcell.style.cssText= 'text-align:center';
-                    newcell.appendChild(newText);
+                    newcell.innerHTML = response.data[i].REFNO
 
                     newcell = newrow.insertCell(6);
-                    newText = document.createTextNode(response.data[i].FULLNAME);
                     newcell.style.cssText= 'text-align:center';
-                    newcell.appendChild(newText);
+                    newcell.innerHTML = response.data[i].FULLNAME
 
                     newcell = newrow.insertCell(7);
-                    newText = document.createTextNode(response.data[i].CDATE);
                     newcell.style.cssText= 'text-align:center';
-                    newcell.appendChild(newText);                                                             
+                    newcell.innerHTML = response.data[i].CDATE
 
                     newcell = newrow.insertCell(8);
-                    newText = document.createTextNode(response.data[i].CLOC);
                     newcell.style.cssText= 'text-align:center';
-                    newcell.appendChild(newText);                                                             
+                    newcell.innerHTML = response.data[i].CLOC
                 }
                 mydes.innerHTML='';
                 mydes.appendChild(myfrag);
                 document.getElementById('fginventory_lblinfo').innerHTML = ttlrows>0 ? '': 'data not found';
                 document.getElementById('fginventory_txt_total').value = numeral(ttlqty).format('0,0');
             }, error : function(xhr, xopt, xthrow){
+                fginventory_btn_gen.disabled = false
+                fginventory_btn_gen.innerHTML = `<i class="fas fa-sync"></i>`
                 alertify.error(xthrow);
             }
         });
