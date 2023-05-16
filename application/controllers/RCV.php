8f5842c2ee32d6209672b03dd024b9a38e38bbd9
Ana <anasuryana10@gmail.com>
2023-05-16 14:11:05 +0700

    Fix : duplicate row

--- application/controllers/RCV.php
+++ application/controllers/RCV.php
@@ -579,10 +579,10 @@ class RCV extends CI_Controller
                     ];
                     $lastLine++;
                 }
-                if (count($datas)) {
-                    $ttlinserted = $this->RCV_mod->insertb($datas);
-                    $myar[] = ['cd' => 1, 'msg' => 'Saved successfully'];
-                }
+            }
+            if (count($datas)) {
+                $ttlinserted = $this->RCV_mod->insertb($datas);
+                $myar[] = ['cd' => 1, 'msg' => 'Saved successfully'];
             }
         } else {
             for ($i = 0; $i < $dataCount; $i++) {
@@ -2674,7 +2674,7 @@ class RCV extends CI_Controller
         $sheet = $spreadsheet->getActiveSheet();
         $sheet->setTitle('SHEETMASUK');
         $sheet->mergeCells('C1:O1');
-        $sheet->setCellValueByColumnAndRow(3, 1, 'Pembukuan Masuk '.$cdoctype);
+        $sheet->setCellValueByColumnAndRow(3, 1, 'Pembukuan Masuk ' . $cdoctype);
         $sheet->getStyle('C1')->getAlignment()->setHorizontal('center');
         $sheet->setCellValueByColumnAndRow(2, 2, 'PT. SMT INDONESIA');
         $sheet->setCellValueByColumnAndRow(2, 3, 'Kawasan EJIP Plot 5C2 Cikarang Selatan');
