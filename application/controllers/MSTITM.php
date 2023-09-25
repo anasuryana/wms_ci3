c0f24b662fbd00cffe6d8775aae5805780d1f9f4
Ana Suryana di PSI <ana.suryana@smt.co.id>
2023-09-25 09:21:50 +0700

    Fix : tidak bisa search data FG Exim

--- application/controllers/MSTITM.php
+++ application/controllers/MSTITM.php
@@ -192,16 +192,10 @@ class MSTITM extends CI_Controller
         $search = $this->input->get('insearch');
         $searchby = $this->input->get('insearchby');
         $rs = [];
-        $responApi = Requests::request('http://192.168.0.29:8080/ems-glue/api/item/search?insearch=' . $search
+        $responApi = Requests::request('http://192.168.0.29:8080/ems-glue/api/item/searchFGExim?insearch=' . $search
             . '&insearchby=' . $searchby, [], [], 'GET', ['timeout' => 900, 'connect_timeout' => 900]);
         $rs = json_decode($responApi->body, true);
-        die($responApi->body);
-        foreach ($rs as &$r) {
-            $r['MITM_NWG'] = substr($r['MITM_NWG'], 0, 1) == '.' ? '0' . $r['MITM_NWG'] : $r['MITM_NWG'];
-            $r['MITM_GWG'] = substr($r['MITM_GWG'], 0, 1) == '.' ? '0' . $r['MITM_GWG'] : $r['MITM_GWG'];
-        }
-        unset($r);
-        die('{"data": ' . json_encode($rs) . '}');
+        die(json_encode($rs));
     }
     public function searchrm_exim()
     {
