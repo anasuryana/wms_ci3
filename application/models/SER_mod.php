ed95763e8e1da51887f7e33cf2fc97ce6e3d20a7
Ana Suryana di PSI <ana.suryana@smt.co.id>
2023-11-10 15:18:23 +0700

    Fix : prevent out-use awip recalculated

--- application/models/SER_mod.php
+++ application/models/SER_mod.php
@@ -449,9 +449,10 @@ class SER_mod extends CI_Model
 
     public function select_validate_emergency($preffno)
     {
-        $this->db->select("SER_ID,VITHDLV.ITH_SER DLVSER,SERD2_SER,isnull(SERC_COMQTY,TQTY) TQTY");
+        $this->db->select("SER_ID,ISNULL(VITHDLV.ITH_SER,VITHWIP.ITH_SER) DLVSER,SERD2_SER,isnull(SERC_COMQTY,TQTY) TQTY");
         $this->db->from($this->TABLENAME);
         $this->db->join("(SELECT * FROM ITH_TBL WHERE ITH_SER='" . $preffno . "' AND ITH_FORM like '%OUT-SHP%') VITHDLV", "SER_ID=VITHDLV.ITH_SER", "left");
+        $this->db->join("(SELECT * FROM ITH_TBL WHERE ITH_SER='" . $preffno . "' AND ITH_FORM like '%OUT-USE%') VITHWIP", "SER_ID=VITHWIP.ITH_SER", "left");
         $this->db->join("(SELECT TOP 1 ITH_SER,ABS(ITH_QTY) TQTY FROM ITH_TBL WHERE ITH_SER='" . $preffno . "') VITH", "SER_ID=VITH.ITH_SER", "left");
         $this->db->join("(SELECT SERD2_SER FROM SERD2_TBL WHERE SERD2_SER='" . $preffno . "' GROUP BY SERD2_SER) VSERD2", "SER_ID=VSERD2.SERD2_SER", "left");
         $this->db->join('SERC_TBL', "SER_ID=SERC_COMID", "left");
