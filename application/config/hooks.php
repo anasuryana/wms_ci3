06fb04bbc4859e5540383cc2a4d0e6cc770b3f5d
Ana Suryana di PSI <ana.suryana@smt.co.id>
2024-01-12 15:29:32 +0700

    build : pembuatan environment

--- application/config/hooks.php
+++ application/config/hooks.php
@@ -11,3 +11,9 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 |	https://codeigniter.com/user_guide/general/hooks.html
 |
 */
+
+
+$hook['pre_system'] = function () {
+    $dotenv = Dotenv\Dotenv::createImmutable(FCPATH);
+    $dotenv->load();
+};
--- index.php
+++ index.php
@@ -53,8 +53,13 @@
  *
  * NOTE: If you change these, also change the error_reporting() code below
  */
-	define('ENVIRONMENT', isset($_SERVER['CI_ENV']) ? $_SERVER['CI_ENV'] : 'development');
-	// define('ENVIRONMENT', 'production');
+	// define('ENVIRONMENT', isset($_SERVER['CI_ENV']) ? $_SERVER['CI_ENV'] : 'development');
+	require_once 'vendor/autoload.php';
+	$dotenv = Dotenv\Dotenv::createImmutable('./', '.env');
+    $dotenv->load();
+
+	putenv("CI_ENV=".$_ENV['CI_ENV']);
+	define('ENVIRONMENT', $_SERVER['CI_ENV']);
 
 /*
  *---------------------------------------------------------------
