e5a461ccdb8378e685959b4b0c4fe960c8973e33
Ana Suryana di PSI <ana.suryana@smt.co.id>
2024-08-02 09:10:05 +0700

    fix : supplied-qty tidak muncul sebagian

--- application/models/SPL_mod.php
+++ application/models/SPL_mod.php
@@ -271,7 +271,7 @@ class SPL_mod extends CI_Model
 
     public function selectkitby4parv($pspl, $pcat, $pline, $pfr)
     {
-        $this->db->select("SPL_ORDERNO,SPL_RACKNO, UPPER(SPL_ITMCD) SPL_ITMCD,MITM_SPTNO, SPL_QTYUSE, SPL_MC, SPL_MS, TTLREQ, TTLSCN");
+        $this->db->select("UPPER(RTRIM(SPL_ORDERNO)) SPL_ORDERNO,SPL_RACKNO, UPPER(RTRIM(SPL_ITMCD)) SPL_ITMCD,MITM_SPTNO, SPL_QTYUSE, SPL_MC, SPL_MS, TTLREQ, TTLSCN");
         $this->db->from("(SELECT SPL_ORDERNO,SPL_RACKNO,aliasrack, SPL_ITMCD, max(SPL_QTYUSE) SPL_QTYUSE,SPL_MS, SPL_MC, SUM(SPL_QTYREQ) TTLREQ, 0 TTLSCN
         FROM $this->TABLENAME LEFT JOIN (select MSTLOC_CD,max(aliasrack) aliasrack  from vinitlocation group by MSTLOC_CD) vloc on SPL_RACKNO=MSTLOC_CD
         WHERE SPL_DOC='$pspl' AND SPL_CAT='$pcat' AND SPL_LINE='$pline' AND SPL_FEDR='$pfr'
