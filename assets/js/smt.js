d27a2724a3a18e3e69692847aa179a7df43f8361
Ana Suryana di PSI <ana.suryana@smt.co.id>
2025-02-26 12:00:06 +0700

    draft : js simulation prod plan

--- assets/js/smt.js
+++ assets/js/smt.js
@@ -1,6 +1,7 @@
 // vkeikaku
 let vkeikakuOperationMode = 0
 let vkeikakuActiveTab = ''
+let keikakuColumnIndexStart = 6
 
 function smtPWValidator(pvalue, data = { PWPOL_LENGTH: 0, PWPOL_ISCOMPLEX: 0 }) {
     const minimumLengthPassword = data.PWPOL_LENGTH
@@ -85,11 +86,11 @@ function doubleControlEvent() {
     if (event.key === 'Control') {
         timesCtrlClicked++
         if (timesCtrlClicked >= 2) {
-            if(typeof keikaku_main_tab !== 'undefined') {
+            if (typeof keikaku_main_tab !== 'undefined') {
                 console.log('tab keikaku terbuka')
-                if(vkeikakuOperationMode) {
-                    vkeikakuActiveTab = vkeikakuActiveTab=='#keikaku_tabRM' ? '#keikaku_tab_prodplan' : '#keikaku_tabRM'
-                
+                if (vkeikakuOperationMode) {
+                    vkeikakuActiveTab = vkeikakuActiveTab == '#keikaku_tabRM' ? '#keikaku_tab_prodplan' : '#keikaku_tabRM'
+
                     let firstTabEl = document.querySelector(`#keikaku_main_tab button[data-bs-target="${vkeikakuActiveTab}"]`)
                     let thetab = new bootstrap.Tab(firstTabEl)
                     thetab.show()
@@ -100,7 +101,7 @@ function doubleControlEvent() {
             }
         }
         setTimeout(() => (timesCtrlClicked = 0), 350)
-    }  
+    }
 }
 
 let timesCtrlClicked = 0;
@@ -108,7 +109,7 @@ document.addEventListener('keyup', doubleControlEvent, true)
 
 function keikaku_btn_mode(pThis) {
     const div_alert = document.getElementById('keikaku-div-operation-alert')
-    if(vkeikakuOperationMode === 1) {
+    if (vkeikakuOperationMode === 1) {
         pThis.innerText = 'User Mode'
         vkeikakuOperationMode = 0
         div_alert.innerHTML = ``
@@ -123,6 +124,170 @@ function keikaku_btn_mode(pThis) {
 }
 
 function vkeikakuSimulate(pData, pCalculation) {
-    console.log({data : pData, calculation : pCalculation})
+    console.log({ data: pData, calculation: pCalculation })
+    let asProdplan1 = [null, null, null, null, null, null];
+    let asProdplan2 = [null, null, null, null, null, null];
+    let asProdplan3 = [null, null, null, null, null, null];
+    let ttlCalculationColumn = pCalculation[9].length
+    for (let i = 1; i < ttlCalculationColumn; i++) {
+        let _jam = String('0' + String(pCalculation[9][i] - 1)).slice(-2)
+        asProdplan1.push(_jam)
+        asProdplan2.push(pCalculation[8][i] * pCalculation[10][i])
+        asProdplan3.push(pCalculation[8][i])
+    }
+    let asMatrix = []
+    asMatrix.push(asProdplan1)
+    asMatrix.push(asProdplan2)
+    asMatrix.push(asProdplan3)
+
+    let tempModel = ''
+    let tempType = '';
+    let tempSpecs = '';
+    let tempAssyCode = '';
+    pData.forEach((AI, index) => {
+        let _shouldChangeModel = false;
+        let _usedTime = 0;
+        if (tempModel.length > 0) {
+            if (AI[5].substring(0, 4) == tempType.substring(0, 4) && tempSpecs == AI[9]) {
+                if (tempAssyCode == AI[7]) {
+                    _shouldChangeModel = false;
+                } else {
+                    _shouldChangeModel = true;
+                    _usedTime = 0.25;
+                }
+            } else {
+                _usedTime = 0.25;
+                _shouldChangeModel = true;
+            }
+
+            if (tempSpecs != AI[9]) {
+                tempSpecs = AI[9];
+            }
+
+            if (AI[5].substring(0, 4) != tempType.substring(0, 4)) {
+                tempType = AI[5];
+            }
+
+            if (tempAssyCode != AI[7]) {
+                tempAssyCode = AI[7];
+            }
+        } else {
+            if (tempModel != AI[1]) {
+                _shouldChangeModel = false; // first row always set to false
+                tempModel = AI[1];
+                tempType = AI[5];
+                tempSpecs = AI[9];
+                tempAssyCode = AI[7];
+            }
+        }
+        let _producton_worktime = AI[10] / 3600 * numeral(AI[4]).value()
+        let _ct_hour = AI[10] / 3600
+        let _asMatrix1 = [null, _shouldChangeModel, (_shouldChangeModel ? _usedTime : 0), null, null, null];
+        let _asMatrix2 = [
+            AI[7],
+            null,
+            _producton_worktime,
+            AI[2] + '-' + AI[7],
+            _ct_hour,
+            AI[9] + "#" + AI[1] + "#" + AI[2] + "#" + numeral(AI[3]).value() + "#" + numeral(AI[4]).value() + "#" + AI[5] + "#" + AI[6] + "#" + index + "#" + keikaku_line_input.value
+        ];
+        for (let i = 1; i < ttlCalculationColumn; i++) {
+            _asMatrix1.push(null)
+            _asMatrix2.push(null)
+        }
+
+        asMatrix.push(_asMatrix1)
+        asMatrix.push(_asMatrix2)
+    })
+
+    // bismillah proses kalkulasi waktu
+    let matrixRowsLength = asMatrix.length;
+    for (let i = 3; i < matrixRowsLength; i++) {
+        for (let col = keikakuColumnIndexStart; col < (6 + 36); col++) {
+            let _totalProductionHours = asMatrix[i][2];
+            if (_totalProductionHours == 0) {
+                asMatrix[i][col] = 0;
+            } else {
+                asMatrix[i][col] = _plotTime(asMatrix, col, i, _totalProductionHours.toFixed(5));
+            }
+        }
+    }
+
+    for (let i = 3; i < matrixRowsLength; i++) {
+        for (let col = keikakuColumnIndexStart; col < (6 + 36); col++) {
+            if (!asMatrix[i][0]) { // change model
+                if (col === keikakuColumnIndexStart) {
+                    if (asMatrix[i][1]) {
+                        if (asMatrix[i][col] == 0) {
+                            asMatrix[i][col] = 0;
+                        } else {
+                            asMatrix[i][col] = Math.round(asMatrix[i][col] / asMatrix[i][2]);
+                        }
+                    } else {
+                        if (asMatrix[i][col] == 0) {
+                            asMatrix[i][col] = 0;
+                        } else {
+                            asMatrix[i][col] = asMatrix[i][col] / asMatrix[i][4];
+                        }
+                    }
+                }
+            } else {
+                if (asMatrix[i][4] == 0) {
+                    asMatrix[i][col] = 0;
+                } else {
+                    if (col === keikakuColumnIndexStart) {
+                        asMatrix[i][col] = Math.round(asMatrix[i][col] / asMatrix[i][4]);
+                    } else {
+                        if (asMatrix[i][col] == 0) {
+                            asMatrix[i][col] = 0;
+                        } else {
+                            asMatrix[i][col] = Math.round(asMatrix[i][col] / asMatrix[i][4]);
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    console.log(asMatrix)
 }
 
+
+function _plotTime(data, parX, parY, parProductionHours) {
+    let _plotedTime = 0;
+    let restEffectiveWorkTime = data[1][parX] - _sumVertical(data, parX, parY);
+
+    if (parX === keikakuColumnIndexStart) {
+        if (parProductionHours < restEffectiveWorkTime) {
+            _plotedTime = parProductionHours;
+        } else {
+            _plotedTime = restEffectiveWorkTime;
+        }
+    } else {
+        if (parProductionHours < restEffectiveWorkTime) {
+            _plotedTime = parProductionHours - _sumHorizontal(data, parX, parY);
+        } else if ((parProductionHours - _sumHorizontal(data, parX, parY)) < restEffectiveWorkTime) {
+            _plotedTime = parProductionHours - _sumHorizontal(data, parX, parY);
+        } else {
+            _plotedTime = restEffectiveWorkTime;
+        }
+    }
+
+    return _plotedTime;
+}
+
+function _sumVertical(data, parX, parY) {
+    let _summarizedVertical = 0;
+    for (let __r = parY; __r > 2; __r--) {
+        _summarizedVertical += data[__r][parX];
+    }
+    return _summarizedVertical.toFixed(5)
+}
+
+function _sumHorizontal(data, parX, parY) {
+    let _summarizedHorizontal = 0;
+    for (let __c = parX; __c >= keikakuColumnIndexStart; __c--) {
+        _summarizedHorizontal += data[parY][__c];
+    }
+    return _summarizedHorizontal;
+}
