e6f7cad72cf32e7a4f53719f8afd21974b74e411
Ana Suryana di PSI <ana.suryana@smt.co.id>
2025-09-25 14:59:23 +0700

    fix : avoid ambiguity

--- application/views/wms/vrcv_customs.php
+++ application/views/wms/vrcv_customs.php
@@ -125,7 +125,7 @@
                 </div>
                 <div class="form-check form-check-inline">
                     <input class="form-check-input" type="radio" name="rcvcustoms_mode" onclick="rcvcustoms_mode_eC(this)" id="rcvcustoms_mode3" value="3">
-                    <label class="form-check-label" for="rcvcustoms_mode3">Material Sample</label>
+                    <label class="form-check-label" for="rcvcustoms_mode3">Material Sample & <span title="Depuruntukan untuk bahan baku dan penolong tetapi tidak diinput di MEGA EMS">Other</span></label>
                 </div>
             </div>
         </div>
